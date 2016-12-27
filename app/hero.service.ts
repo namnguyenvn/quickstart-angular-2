d19eb978fbf4fb8800934b7c03ee3bcd86954037
NamNguyen <namnguyenvn@gmail.com>
2016-12-27 20:52:25 +0700

    Half way HTTP, before move to Observables https://angular.io/docs/ts/latest/tutorial/toh-pt6.html

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,16 +1,73 @@
 import { Injectable } from '@angular/core';
 
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise'; // ?? the Angular Observable doesn't have a toPromise operator => add this
+
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+// import { HEROES } from './mock-heroes'; use In Memory Web API Instead
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'api/heroes';
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  constructor(private http: Http) {}
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+                .toPromise() // ?? convert Observable to Promise
+                .then(response => response.json().data as Hero[])
+                .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.Message || error);
   }
 
   getHero(id: number): Promise<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get(url)
+                .toPromise()
+                .then(response => response.json().data as Hero)
+                .catch(this.handleError);
+  }
+
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+
+    return this.http
+          .put(url, JSON.stringify(hero), {headers: this.headers})
+          .toPromise()
+          .then(() => hero) // ?? why () =>
+          .catch(this.handleError);
+  }
+
+  create(name: string): Promise<Hero> {
+    return this.http
+          .post(this.heroesUrl, JSON.stringify({name: name}),{headers: this.headers})
+          .toPromise()
+          .then(res => res.json().data) // ?? is this the response data that will be used in client
+          .catch(this.handleError);
+  }
+
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete(url, {headers: this.headers})
+          .toPromise()
+          .then(() => null)
+          .catch(this.handleError);
+  }
+
+  /* V1:
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }*/
+
+  /* V1:
+  getHero(id: number): Promise<Hero> {
     return this.getHeroes()
               .then(heroes => heroes.find(hero => hero.id === id));
-  }
+  }*/
 }
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -38,4 +38,24 @@ export class HeroesComponent implements OnInit {
   gotoDetail(): void {
     this.router.navigate(['/detail', this.selectedHero.id]);
   }
+
+  add(name: string): void {
+    name = name.trim();
+
+    if (!name) { return; }
+    this.heroService.create(name)
+        .then(hero => { // ?? why hero here: possible: the create function return a Hero type object, the then callback function catch it, then push into the 2 way binding model (to update the list in client)
+          this.heroes.push(hero);
+          this.selectedHero = null;
+        })
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+        .delete(hero.id)
+        .then(() => {
+          this.heroes = this.heroes.filter(h => h !== hero); // remove deleted hero from 2 way binding model
+          if (this.selectedHero === hero) { this.selectedHero = null; } // remove selected flag
+        });
+  }
 }
