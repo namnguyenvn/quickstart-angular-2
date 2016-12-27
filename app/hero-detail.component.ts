a0d18d05aa53a9efe1a04308748ad0a878bb1bd9
NamNguyen <namnguyenvn@gmail.com>
2016-12-27 17:33:50 +0700

    Finished Routing part: https://angular.io/docs/ts/latest/tutorial/toh-pt5.html

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,23 +1,47 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import 'rxjs/add/operator/switchMap';
 
 
 @Component({
+  moduleId: module.id,
   selector: 'my-hero-detail',
-  template: `
-            <div *ngIf="hero">
-              <h2>{{hero.name}} details!</h2>
-              <div><label>id: </label>{{hero.id}}</div>
-              <div>
-                <label>name: </label>
-                <input [(ngModel)]="hero.name" placeholder="name"/>
-              </div>
-            </div>
-          `
-
+  templateUrl: 'hero-detail.component.html',
+  styleUrls: [ 'hero-detail.component.css' ]
 })
 
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
   @Input()
   hero: Hero
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+  /** ??
+  read again Observation
+  Note how the switchMap operator maps the id in the observable route parameters to a new Observable, the result of the HeroService.getHero method.
+
+  If the user re-navigates to this component while a getHero request is still inflight, switchMap cancels that old request before calling HeroService.getHero again.
+
+  The hero id is a number. Route parameters are always strings. So we convert the route parameter value to a number with the JavaScript (+) operator.
+
+  The Router manages the observables it provides and localizes the subscriptions. The subscriptions are cleaned up when the component is destroyed, protecting against memory leaks, so we don't need to unsubscribe from the route params Observable
+
+  https://angular.io/docs/ts/latest/guide/router.html#!#activated-route
+  */
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
